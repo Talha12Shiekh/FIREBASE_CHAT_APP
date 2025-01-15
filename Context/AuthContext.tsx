0ff73e7ac7c3d4d88901d29d582e7ec786605f62
Talha Khurram <tk.shiekh4567@gmail.com>
2025-01-15 04:37:35 -0800

    Pending adding Navigation and Authentication

---
+++ Context/AuthContext.tsx
@@ -0,0 +1,73 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+
+interface AuthContextProps {
+    children: React.ReactNode
+}
+
+
+interface ContextProviderProps {
+    user: null,// pending,
+    userAuthenticated: boolean,
+    login: (email: string, password: number) => Promise<void>,
+    logout: (email: string, password: number) => Promise<void>,
+    register: (email: string, password: number, username: string, profileURL: string) => Promise<void>,
+}
+export const AuthContext = createContext<ContextProviderProps | null>(null);
+
+export const AuthContextProvider = ({ children }: AuthContextProps) => {
+    const [user, setuser] = useState(null);
+    const [userAuthenticated, setuserAuthenticated] = useState(false);
+
+    useEffect(() => {
+        // onAuthStateChanged()
+        setTimeout(() => {
+            setuserAuthenticated(true);
+        },3000)
+    }, []);
+
+    const login = async (email: string, password: number) => {
+        try {
+
+            // pending
+        } catch (error) {
+
+
+        }
+    }
+
+    const logout = async (email: string, password: number) => {
+        try {
+
+            // pending
+        } catch (error) {
+
+        }
+    }
+
+    const register = async (email: string, password: number, username: string, profileURL: string) => {
+        try {
+
+            // pending
+        } catch (error) {
+
+        }
+    }
+
+
+    const contextreturnvalue: ContextProviderProps = { user, userAuthenticated, login, logout, register };
+
+
+    return <AuthContext.Provider value={contextreturnvalue}>
+        {children}
+    </AuthContext.Provider>
+}
+
+export const useAuth = () => {
+    const value = useContext(AuthContext);
+
+    if(!value){
+        throw new Error("Component must be wrapped inside a provider !");
+    }
+    return value;
+}
